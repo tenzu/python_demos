ea5eeb6289ddacbeae16b21802f72b40f167af19
tenzu <huangxin.cliff@gmail.com>
2017-11-17 13:23:57 +0800

    tmp_CSYS coordinates

---
+++ 3D_aggregate/tmp_CSYS.py
@@ -0,0 +1,19 @@
+import random
+i,j = 0,0
+tmp_list = []
+tmp_CSYS = []
+f = open('tmp_CSYS.txt','w')
+while i <= 2:
+	while j <= 5:
+		if j <= 2:
+			tmp_list.append(random.uniform(-100,100))
+		else:
+			tmp_list.append(random.uniform(0,360))
+		j+=1
+	tmp_CSYS.append(tmp_list)
+	for k in tmp_list:
+		f.write('%3.3f' % k + '\t')
+	f.write('\n')
+	tmp_list = []
+	j=1
+	i+=1
