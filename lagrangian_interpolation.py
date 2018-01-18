3549d881fbb7338f139e1b1ce3b8065e58488ce5
tenzu <huangxin.cliff@gmail.com>
2018-01-18 12:51:53 +0800

    pi.py commit

--- lagrangian_interpolation.py
+++ lagrangian_interpolation.py
@@ -1,17 +1,17 @@
-#coding:utf-8
+# coding:utf-8
 from scipy import interpolate
 import matplotlib.pyplot as plt
 
-x0,y0 = [1,2,3,4],[1,8,27,64]
-#x1,y1 = [float(i) for i in x0],[float(i) for i in y0]
-x1,y1 = list(map(float,x0)),list(map(float,y0))
-x = 5.5 
-print('The interpolation result is:',interpolate.lagrange(x1,y1)(x))
-print('The result of x^3 is:',x**3)
+x0, y0 = [1, 2, 3, 4], [1, 8, 27, 64]
+# x1,y1 = [float(i) for i in x0],[float(i) for i in y0]
+x1, y1 = list(map(float, x0)), list(map(float, y0))
+x = 5.5
+print('The interpolation result is:', interpolate.lagrange(x1, y1)(x))
+print('The result of x^3 is:', x ** 3)
 
 plt.title('Lagrangian Interpolation')
 plt.xlabel(u'X-axis')
 plt.ylabel(u'Y-axis')
-plt.plot(x0,y0,'or--')
-plt.plot(x,interpolate.lagrange(x1,y1)(x),'*b')
+plt.plot(x0, y0, 'or--')
+plt.plot(x, interpolate.lagrange(x1, y1)(x), '*b')
 plt.show()
