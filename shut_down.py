f82081a146905c86c61d42a2372244f33acbffa6
Tenzu <huangxin.cliff@gmail.com>
2019-04-12 22:25:41 +0800

    shutdown_py

--- shut_down.py
+++
@@ -1,4 +0,0 @@
-import os
-time = input('The system will be shutdown after XX seconds: ')
-command = r'shutdown /s /t %s' % time
-os.system(command)
---
+++ shut_down_py.py
@@ -0,0 +1,4 @@
+import os
+time = int(input('The system will be shutdown after XX minutes: '))*60
+command = r'shutdown /s /t %s' % time
+os.system(command)
