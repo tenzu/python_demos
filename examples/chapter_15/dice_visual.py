bbc128210d9eae68de0a055d68afb2a7912fc04d
Tenzu <huangxin.cliff@gmail.com>
2019-04-25 10:51:57 +0800

    chapter 15 finished

--- examples/chapter_15/dice_visual.py
+++ examples/chapter_15/dice_visual.py
@@ -11,14 +11,14 @@ results = []
 for roll_num in range(1000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
-    
+
 # Analyze the results.
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result+1):
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
-    
+
 # Visualize the results.
 hist = pygal.Bar()
 
