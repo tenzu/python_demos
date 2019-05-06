877a90798371d80d0e0f5a0d1a394980ce5bc57b
Tenzu <huangxin.cliff@gmail.com>
2019-05-06 21:23:06 +0800

    Update cold_call.py

--- cold_call/cold_call.py
+++ cold_call/cold_call.py
@@ -6,12 +6,16 @@ stulist = []
 for line in f1.readlines():
     try:
         if line[:-1].strip():
-            stulist.append(line.split('\t')[2] + '    ' + line.split('\t')[3])
+            if line.split()[3] == '重修':  # skip '重修'
+                continue
+            else:
+                stulist.append(
+                    line.split('\t')[2] + '    ' + line.split('\t')[3])
     except ValueError:
-        if line.split()[0] == '序号':
+        if line.split()[0] == '序号':  # skip title (if it exists)
             continue
         else:
-            print('Abnormal data!!!')
+            print('Abnormal data!!!')  # skip '缓考', '缺考', etc.
     else:
         continue
 r_num = random.randint(5, 12)
