4bf38407969f7264ec41cc9e17a12302fc6a6e5a
Tenzu <huangxin.cliff@gmail.com>
2019-05-09 12:44:39 +0800

    minor updates

--- spline_interpolation.py
+++ spline_interpolation.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 10, num=11, endpoint=True)
 y = np.cos(-x**2 / 9.0)
 f = interp1d(x, y)
-# f2 = interp1d(x, y, kind='cubic') # curve fitting by cubic polynomial
-f2 = interp1d(x, y, 3)
+f1 = interp1d(x, y, kind='nearest')
+f2 = interp1d(x, y, kind='cubic')
+f3 = interp1d(x, y, kind='quadratic')
 
 xnew = np.linspace(0, 10, num=41, endpoint=True)
-plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
-plt.legend(['data', 'linear', 'cubic'], loc='best')
+plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f1(xnew), '--', xnew, f2(xnew),
+         '.-', xnew, f3(xnew), '.--')
+plt.legend(['data', 'linear', 'nearest', 'cubic', 'quadratic'], loc='best')
 plt.show()
