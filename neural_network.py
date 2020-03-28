a7f6141d0fdf65d1087a207c1b9561d62fd9c686
Tenzu <huangxin.cliff@gmail.com>
2020-03-28 19:43:20 +0800

    Create neural_network.py

---
+++ neural_network.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot
+import numpy
+# neural network class defination
+
+
+class neuralNetwork:
+    # initialize the neural network
+    def __iit__():
+        pass
+
+    # train the neural network
+    def train():
+        pass
+
+    # query the neural network
+    def query():
+        pass
