43b29fbb468589c5763aefc0cdbcf143f90efb50
Tenzu <huangxin.cliff@gmail.com>
2020-03-31 13:59:57 +0800

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -5,7 +5,14 @@ import numpy
 
 class neuralNetwork:
     # initialize the neural network
-    def __iit__():
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # learning rate
+        self.lr = learningrate
         pass
 
     # train the neural network
@@ -15,3 +22,11 @@ class neuralNetwork:
     # query the neural network
     def query():
         pass
+
+
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+learning_rate = 0.5
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+print(numpy.random.rand(3, 3) - 0.5)
