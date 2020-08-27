a6f9046636abc997248e1d7c1294afc3d072584c
Tenzu <huangxin.cliff@gmail.com>
2020-08-27 20:44:02 +0800

    Update mat_multiply.py

--- neural_network/mat_multiply.py
+++ neural_network/mat_multiply.py
@@ -3,8 +3,13 @@ from math import exp
 I = np.array([0.9, 0.1, 0.8])
 W1 = np.array([[0.9, 0.3, 0.4], [0.2, 0.8, 0.2], [0.1, 0.5, 0.6]])
 X1 = np.matmul(W1, I)
-print(X1)
-print(type(X1))
+
+
+def sigmoid(x):
+    return 1/(1+exp(-x))
+
+
 for i in range(len(X1)):
-    X1[i] = 1 / (1 + exp(-X1[i]))
+    #X1[i] = 1 / (1 + exp(-X1[i]))
+    X1[i] = sigmoid(X1[i])
 print(X1)
