26d9aa1ed4ca3a68c8e5e30bd608bd9036480a85
Tenzu <huangxin.cliff@gmail.com>
2021-01-14 11:58:49 +0800

    update scores.py

--- scores/scores.py
+++ scores/scores.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.function_base import percentile
 import pygal
 import os
 
@@ -23,6 +24,7 @@ def mainProc():
     exam_performances = []  # final exam performance
     final_scores = []  # score summary
     frequencies = [0, 0, 0, 0, 0]  # frequencies of score levels
+    percentage = [0, 0, 0, 0, 0]  # percentage of score levels
     scores_0 = []  # scores of level 1
     scores_1 = []  # scores of level 2
     scores_2 = []  # scores of level 3
@@ -64,6 +66,8 @@ def mainProc():
         else:
             print("Score error!")
             break
+    for i in range(len(percentage)):
+        percentage[i] = frequencies[i]/len(stu_numbers)
 
     mean = [
         np.mean(scores_0),
@@ -82,31 +86,30 @@ def mainProc():
 
     groupName['group' + txt[:4]] = [
         stu_numbers, stu_names, usual_performances, exam_performances,
-        final_scores, frequencies, scores_0, scores_1, scores_2, scores_3,
+        final_scores, percentage, scores_0, scores_1, scores_2, scores_3,
         scores_4, mean, std
     ]
     groupList.append(groupName['group' + txt[:4]])
 
 
 def barChart1Plotting():
-    print('Now plotting bar chart 1.')
     final_score_bar1 = pygal.Bar()
-    final_score_bar1.title = "Final score intervals"
+    final_score_bar1.title = "最终成绩区间 (%)"
+    # final_score_bar1.title = "Final score intervals (%)"
     final_score_bar1.x_labels = [
-        'Fail', '60 - 70', '70 - 80', '80 - 90', '90 - 100'
+        '不及格', '60 - 70', '70 - 80', '80 - 90', '90 - 100'
     ]
     final_score_bar1._x_title = "Final score intervals"
     final_score_bar1._y_title = "Final score frequency"
     for txt in txtFiles:
         final_score_bar1.add(
-            "Sub-total " + txt[:4], groupList[txtFiles.index(txt)][5])
+            txt[:4] + ' ' + str(len(groupList[txtFiles.index(txt)][0])) + "人", groupList[txtFiles.index(txt)][5])
         final_score_bar1.render_to_file('Final_score_bar1.svg')
 
 
 def barChart2Plotting():
-    print('Now plotting bar chart 2.')
     final_score_bar2 = pygal.Bar()
-    final_score_bar2.title = "Final score means and standard deviations"
+    final_score_bar2.title = "最终成绩均值和标准差"
     final_score_bar2.x_labels = [
         'Fail', '60 - 70', '70 - 80', '80 - 90', '90 - 100'
     ]
@@ -114,14 +117,13 @@ def barChart2Plotting():
     final_score_bar2._y_title = "Final score means and STD.s"
     for txt in txtFiles:
         final_score_bar2.add(
-            "Mean " + txt[:4], groupList[txtFiles.index(txt)][11])
+            "均值 " + txt[:4], groupList[txtFiles.index(txt)][11])
         final_score_bar2.add(
-            "STD " + txt[:4], groupList[txtFiles.index(txt)][12])
+            "标准差 " + txt[:4], groupList[txtFiles.index(txt)][12])
     final_score_bar2.render_to_file('Final_score_bar2.svg')
 
 
 def pieChartPlotting():
-    print('Now plotting pie chart.')
     final_score_pie = pygal.Pie()
     final_score_pie.title = "Final score percentage (%)"
     for i in range(len(groupList)):
@@ -141,7 +143,7 @@ def pieChartPlotting():
 fileOpen()
 for txt in txtFiles:
     print(txt + " is under processing.")
-    f = open(txt, 'r')
+    f = open(txt, 'r', encoding='UTF-8')
     mainProc()
     f.close()
     print(txt + ' finished.')
