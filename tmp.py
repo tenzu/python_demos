351280d6834728f032181fbee1a06334ffcf5720
Tenzu <huangxin.cliff@gmail.com>
2022-06-13 14:55:53 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,70 +1,50 @@
 # coding:utf-8
-# 获得前 n 个主成分
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
 
-X = np.empty((100, 2))
-X[:, 0] = np.random.uniform(0., 100., size=100)
-# 两个特征之间存在线性关系，降维效果明显
-X[:, 1] = 0.75 * X[:, 0] + 3 + np.random.normal(0, 10., size=100)
-plt.scatter(X[:, 0], X[:, 1])
+digits = datasets.load_digits()
+X = digits.data
+y = digits.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
+
+knn_clf1 = KNeighborsClassifier(n_jobs=-1)
+knn_clf1.fit(X_train, y_train)
+print('The kNN score is:\n', knn_clf1.score(X_test, y_test))
+
+# 64个主成分
+pca = PCA(n_components=X_train.shape[1])
+pca.fit(X_train)
+X_train_reduction = pca.transform(X_train)
+X_test_reduction = pca.transform(X_test)
+knn_clf2 = KNeighborsClassifier(n_jobs=-1)
+knn_clf2.fit(X_train_reduction, y_train)
+print('The PCA score is:\n', knn_clf2.score(X_test_reduction, y_test))
+# 主成分所解释的方差
+print('主成分所解释的方差:\n', pca.explained_variance_ratio_)
+# 前 i 个主成分保留下来的方差信息比例曲线
+plt.plot([i for i in range(X_train.shape[1])], [
+    np.sum(pca.explained_variance_ratio_[:i + 1])
+    for i in range(X_train.shape[1])
+])
 plt.show()
+# 指定保留 95% 的方差信息
+pca = PCA(0.95)
+pca.fit(X_train)
+print('保留 95% 方差信息时对应的主成分个数:\n', pca.n_components_)
+# score 低于选取64个主成分的情况，但效率会高很多
+X_train_reduction = pca.transform(X_train)
+X_test_reduction = pca.transform(X_test)
+knn_clf2 = KNeighborsClassifier(n_jobs=-1)
+knn_clf2.fit(X_train_reduction, y_train)
+print('The PCA score is:\n', knn_clf2.score(X_test_reduction, y_test))
 
-
-def demean(X):
-    return X - np.mean(X, axis=0)  # 已经向量化，无需 for 循环
-
-
-X = demean(X)
-
-
-def f(w, X):
-    return np.sum((X.dot(w)**2)) / len(X)
-
-
-# 根据数学公式求梯度
-def df(w, X):
-    return X.T.dot(X.dot(w)) * 2. / len(X)
-
-
-def direction(w):
-    return w / np.linalg.norm(w)
-
-
-def first_component(X, initial_w, eta, n_iters=1e4, epsilon=1e-8):
-    w = direction(initial_w)
-    cur_iter = 0
-    while cur_iter < n_iters:
-        gradient = df(w, X)
-        last_w = w
-        w = w + eta * gradient
-        w = direction(w)
-        if (abs(f(w, X) - f(last_w, X)) < epsilon):
-            break
-        cur_iter += 1
-    return w
-
-
-initial_w = np.random.random(X.shape[1])
-eta = 0.001
-w = first_component(X, initial_w, eta)
-# 去掉第一主成分的分量
-X2 = X - X.dot(w).reshape(-1, 1) * w
-plt.scatter(X2[:, 0], X2[:, 1])
+pca = PCA(n_components=2)
+pca.fit(X)
+X_reduction = pca.transform(X)
+for i in range(10):
+    plt.scatter(X_reduction[y == i, 0], X_reduction[y == i, 1], alpha=0.8)
 plt.show()
-
-
-def first_n_components(n, X, eta=0.01, n_iters=1e4, epsilon=1e-8):
-    X_pca = X.copy()
-    X_pca = demean(X_pca)
-    res = []
-    for i in range(n):
-        initial_w = np.random.random(X_pca.shape[1])
-        w = first_component(X_pca, initial_w, eta)
-        res.append(w)
-        X_pca = X_pca - X_pca.dot(w).reshape(-1, 1) * w
-    return res
-
-
-# 第二主成分
-print('The 2 principal components are:\n', first_n_components(2, X))
