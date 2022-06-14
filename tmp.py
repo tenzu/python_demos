d48b7a00ecdf53f637559e80cb40e6aab49bbe02
Tenzu <huangxin.cliff@gmail.com>
2022-06-14 12:22:44 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -2,23 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
 
-# 生成样本特征和标签（非线性关系）
-x = np.random.uniform(-3, 3, size=100)
-X = x.reshape(-1, 1)
-print('原始矩阵特征:\n', X.shape)
-y = 2 + x + 0.5 * x**2 + np.random.normal(0, 1, 100)
-
-poly = PolynomialFeatures(degree=2)  # 最高次为2
+X = np.arange(1, 11).reshape(-1, 2)
+poly = PolynomialFeatures(degree=2)
 poly.fit(X)
 X2 = poly.transform(X)
-print('现有矩阵特征:\n', X2.shape)  # X 的 0,1,2 次方特征
-lin_reg2 = LinearRegression()
-lin_reg2.fit(X2, y)
-y_predict2 = lin_reg2.predict(X2)
-print('节距:\n', lin_reg2.intercept_)  # y 中指定为 2
-print('多项式系数:', lin_reg2.coef_)    # y 中指定为 0, 1, 0.5
-plt.scatter(x, y)
-plt.plot(np.sort(x), y_predict2[np.argsort(x)], color='r')
-plt.show()
+print('原始数据的样本数和特征数:\n', X.shape)
+print('原始特征:\n', X)
+
+print('升维后数据的样本数和特征数:\n', X2.shape)
+# 零次幂特征，原第1列特征，原第2列特征，原第1列特征平方，原第1列特征乘以第2列特征，原第2列特征平方
+# 自动补全未列出的二次项特征
+# degree=3 时特征为10个
+print('升维后特征:\n', X2)
