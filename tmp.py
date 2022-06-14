b0e2341da8729ab0cc2635a41a9714ffb7e90aa0
Tenzu <huangxin.cliff@gmail.com>
2022-06-14 12:32:15 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,17 +1,19 @@
 # coding:utf-8
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
-X = np.arange(1, 11).reshape(-1, 2)
-poly = PolynomialFeatures(degree=2)
-poly.fit(X)
-X2 = poly.transform(X)
-print('原始数据的样本数和特征数:\n', X.shape)
-print('原始特征:\n', X)
-
-print('升维后数据的样本数和特征数:\n', X2.shape)
-# 零次幂特征，原第1列特征，原第2列特征，原第1列特征平方，原第1列特征乘以第2列特征，原第2列特征平方
-# 自动补全未列出的二次项特征
-# degree=3 时特征为10个
-print('升维后特征:\n', X2)
+x = np.random.uniform(-3, 3, size=100)
+X = x.reshape(-1, 1)
+y = 0.5 * x**2 + x + 2 + np.random.normal(0, 1, 100)
+poly_reg = Pipeline([("poly", PolynomialFeatures(degree=2)),
+                     ("std_scaler", StandardScaler()),
+                     ("lin_reg", LinearRegression())])
+poly_reg.fit(X, y)
+y_predict = poly_reg.predict(X)
+plt.scatter(x, y)
+plt.plot(np.sort(x), y_predict[np.argsort(x)], color='r')
+plt.show()
