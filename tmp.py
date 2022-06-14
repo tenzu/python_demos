859af797889435261801f4a40f8caf68e3888768
Tenzu <huangxin.cliff@gmail.com>
2022-06-14 12:09:30 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,29 +1,24 @@
 # coding:utf-8
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
 # 生成样本特征和标签（非线性关系）
 x = np.random.uniform(-3, 3, size=100)
 X = x.reshape(-1, 1)
-y = 0.5 * x**2 + x + 2 + np.random.normal(0, 1, 100)
-plt.scatter(x, y)
-plt.show()
+print('原始矩阵特征:\n', X.shape)
+y = 2 + x + 0.5 * x**2 + np.random.normal(0, 1, 100)
 
-# 线性回归结果
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
-y_predict = lin_reg.predict(X)
-plt.scatter(x, y)
-plt.plot(x, y_predict, color='r')
-plt.show()
-
-# 手动添加特征后仍然适用线性回归
-X2 = np.hstack([X, X**2])
+poly = PolynomialFeatures(degree=2)  # 最高次为2
+poly.fit(X)
+X2 = poly.transform(X)
+print('现有矩阵特征:\n', X2.shape)  # X 的 0,1,2 次方特征
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X2, y)
 y_predict2 = lin_reg2.predict(X2)
-print('X 和 X^2 系数:', lin_reg2.coef_)
+print('节距:\n', lin_reg2.intercept_)  # y 中指定为 2
+print('多项式系数:', lin_reg2.coef_)    # y 中指定为 0, 1, 0.5
 plt.scatter(x, y)
-plt.plot(np.sort(x), y_predict2[np.argsort(x)], color='r')  # 需要对 x 排序
+plt.plot(np.sort(x), y_predict2[np.argsort(x)], color='r')
 plt.show()
