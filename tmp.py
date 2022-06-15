fc322a84b654fb8e1171b96ee3ae19e323ba7a0f
Tenzu <huangxin.cliff@gmail.com>
2022-06-15 10:40:53 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.pipeline import Pipeline
@@ -11,18 +11,16 @@ np.random.seed(666)
 x = np.random.uniform(-3.0, 3.0, size=100)
 X = x.reshape(-1, 1)
 y = 0.5 * x**2 + x + 2 + np.random.normal(0, 1, size=100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
 
 # 使用线性回归
 lin_reg = LinearRegression()
-lin_reg.fit(X, y)
-lin_reg.score(X, y)
-y_predict = lin_reg.predict(X)
-print('The score is:\n', lin_reg.score(X, y))
-print('The MSE of linear regression is:\n', mean_squared_error(y, y_predict))
-print('评分低, MSE高, 说明不适合使用线性回归。')
-plt.scatter(x, y)
-plt.plot(np.sort(x), y_predict[np.argsort(x)], color='r')
-plt.show()
+
+# 线性回归 MSE
+lin_reg.fit(X_train, y_train)
+y_predict = lin_reg.predict(X_test)
+print('The MSE of linear regression is:\n',
+      mean_squared_error(y_test, y_predict))
 
 
 # 使用多项式回归
@@ -31,36 +29,18 @@ def PolynomialRegression(degree):
                      ("std_scaler", StandardScaler()),
                      ("lin_reg", LinearRegression())])
 
-
-# 2项式回归
+# 2次项式回归
 poly2_reg = PolynomialRegression(degree=2)
-poly2_reg.fit(X, y)
-y2_predict = poly2_reg.predict(X)
-print('The MSE of degree=2 is:\n', mean_squared_error(y, y2_predict))
-plt.scatter(x, y)
-plt.plot(np.sort(x), y2_predict[np.argsort(x)], color='r')
-plt.show()
-print('曲线只包含预测点数据，并非拟合出的曲线！')
-# 10项式回归
+poly2_reg.fit(X_train, y_train)
+y2_predict = poly2_reg.predict(X_test)
+print('The MSE of degree=2 is:\n', mean_squared_error(y_test, y2_predict))
+# 10次项式回归
 poly10_reg = PolynomialRegression(degree=10)
-poly10_reg.fit(X, y)
-y10_predict = poly10_reg.predict(X)
-print('The MSE of degree=10 is:\n', mean_squared_error(y, y10_predict))
-plt.scatter(x, y)
-plt.plot(np.sort(x), y10_predict[np.argsort(x)], color='r')
-plt.show()
-# 100项式回归
+poly10_reg.fit(X_train, y_train)
+y10_predict = poly10_reg.predict(X_test)
+print('The MSE of degree=10 is:\n', mean_squared_error(y_test, y10_predict))
+# 100次项式回归
 poly100_reg = PolynomialRegression(degree=100)
-poly100_reg.fit(X, y)
-y100_predict = poly100_reg.predict(X)
-print('The MSE of degree=100 is:\n', mean_squared_error(y, y100_predict))
-plt.scatter(x, y)
-plt.plot(np.sort(x), y100_predict[np.argsort(x)], color='r')
-plt.show()
-# 还原真正的拟合曲线
-X_plot = np.linspace(-3, 3, 100).reshape(100, 1)
-y_plot = poly100_reg.predict(X_plot)
-plt.scatter(x, y)
-plt.plot(X_plot[:, 0], y_plot, color='r')
-plt.axis([-3, 3, 0, 10])
-plt.show()
+poly100_reg.fit(X_train, y_train)
+y100_predict = poly100_reg.predict(X_test)
+print('The MSE of degree=10 is:\n', mean_squared_error(y_test, y100_predict))
