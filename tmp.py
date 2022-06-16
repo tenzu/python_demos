8c5f7693fcf19f8ec45d4fae13523c3ef7af2364
Tenzu <huangxin.cliff@gmail.com>
2022-06-16 09:56:34 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,47 +1,50 @@
 # coding:utf-8
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
+# 手动指定数据集
 np.random.seed(666)
 x = np.random.uniform(-3.0, 3.0, size=100)
 X = x.reshape(-1, 1)
 y = 0.5 * x**2 + x + 2 + np.random.normal(0, 1, size=100)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=10)
 
-# 使用线性回归
-lin_reg = LinearRegression()
+# 分类算法学习曲线
+def plot_learning_curve(algo, X_train, X_test, y_train, y_test):
+    train_score = []
+    test_score = []
+    for i in range(1, len(X_train)+1):
+        algo.fit(X_train[:i], y_train[:i])
+        y_train_predict = algo.predict(X_train[:i])
+        train_score.append(mean_squared_error(y_train[:i], y_train_predict))
+        y_test_predict = algo.predict(X_test)
+        test_score.append(mean_squared_error(y_test, y_test_predict))
+    plt.plot([i for i in range(1, len(X_train)+1)], 
+                               np.sqrt(train_score), label="train")
+    plt.plot([i for i in range(1, len(X_train)+1)], 
+                               np.sqrt(test_score), label="test")
+    plt.legend()
+    plt.axis([0, len(X_train)+1, 0, 4])   # 限定了坐标轴显示范围
+    plt.show()
 
-# 线性回归 MSE
-lin_reg.fit(X_train, y_train)
-y_predict = lin_reg.predict(X_test)
-print('The MSE of linear regression is:\n',
-      mean_squared_error(y_test, y_predict))
+plot_learning_curve(LinearRegression(), X_train, X_test, y_train, y_test)
 
 
-# 使用多项式回归
+# 2阶多项式回归学习曲线
 def PolynomialRegression(degree):
-    return Pipeline([("poly", PolynomialFeatures(degree=degree)),
-                     ("std_scaler", StandardScaler()),
-                     ("lin_reg", LinearRegression())])
+    return Pipeline([
+        ("poly", PolynomialFeatures(degree=degree)),
+        ("std_scaler", StandardScaler()),
+        ("lin_reg", LinearRegression())
+    ])
 
-# 2次项式回归
 poly2_reg = PolynomialRegression(degree=2)
-poly2_reg.fit(X_train, y_train)
-y2_predict = poly2_reg.predict(X_test)
-print('The MSE of degree=2 is:\n', mean_squared_error(y_test, y2_predict))
-# 10次项式回归
-poly10_reg = PolynomialRegression(degree=10)
-poly10_reg.fit(X_train, y_train)
-y10_predict = poly10_reg.predict(X_test)
-print('The MSE of degree=10 is:\n', mean_squared_error(y_test, y10_predict))
-# 100次项式回归
-poly100_reg = PolynomialRegression(degree=100)
-poly100_reg.fit(X_train, y_train)
-y100_predict = poly100_reg.predict(X_test)
-print('The MSE of degree=10 is:\n', mean_squared_error(y_test, y100_predict))
+plot_learning_curve(poly2_reg, X_train, X_test, y_train, y_test)
+
 
