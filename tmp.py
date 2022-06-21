2e8e92ae536f54167763afc6dfe4d321f0392fa8
Tenzu <huangxin.cliff@gmail.com>
2022-06-21 10:28:28 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,7 +1,12 @@
 # coding:utf-8
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from matplotlib.colors import ListedColormap
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 np.random.seed(666)
 X = np.random.normal(0, 1, size=(200, 2))
@@ -11,3 +16,39 @@ for _ in range(20):
 plt.scatter(X[y == 0, 0], X[y == 0, 1])
 plt.scatter(X[y == 1, 0], X[y == 1, 1])
 plt.show()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
+
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train)
+print('未处理数据时的训练集评分:', log_reg.score(X_train, y_train))
+print('未处理数据时的测试集评分:', log_reg.score(X_test, y_test))
+
+
+def plot_decision_boundary(model, axis):
+    x0, x1 = np.meshgrid(
+        np.linspace(axis[0], axis[1], int(
+            (axis[1] - axis[0]) * 100)).reshape(-1, 1),
+        np.linspace(axis[2], axis[3], int(
+            (axis[3] - axis[2]) * 100)).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
+    plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
+# 正则化前的决策边界
+plot_decision_boundary(log_reg, axis=[-4, 4, -4, 4])
+plt.scatter(X[y==0,0], X[y==0,1])
+plt.scatter(X[y==1,0], X[y==1,1])
+plt.show()
+
+def PolynomialLogisticRegression(degree):
+    return Pipeline([
+        ('poly', PolynomialFeatures(degree=degree)),
+        ('std_scaler', StandardScaler()),
+        ('log_reg', LogisticRegression())
+    ])
+poly_log_reg = PolynomialLogisticRegression(degree=2)
+poly_log_reg.fit(X_train, y_train)
+print('训练集评分:',poly_log_reg.score(X_train, y_train))
+print('测试集评分:',poly_log_reg.score(X_test, y_test))
