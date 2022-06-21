220e5786596d77cf1c0ee1bc950925d141ccbc61
Tenzu <huangxin.cliff@gmail.com>
2022-06-21 09:50:28 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,9 +1,13 @@
 # coding:utf-8
-# 逻辑回归
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+np.random.seed(666)
+X = np.random.normal(0, 1, size=(200, 2))
+y = np.array((X[:, 0]**2 + X[:, 1]) < 1.5, dtype='int')
+for _ in range(20):
+    y[np.random.randint(200)] = 1
+plt.scatter(X[y == 0, 0], X[y == 0, 1])
+plt.scatter(X[y == 1, 0], X[y == 1, 1])
+plt.show()
