54cf66b2b4134e6096c26231973284ca759a2ded
Tenzu <huangxin.cliff@gmail.com>
2022-06-22 11:49:47 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -13,17 +13,19 @@ X = np.random.normal(0, 1, size=(200, 2))
 y = np.array((X[:, 0]**2 + X[:, 1]) < 1.5, dtype='int')
 for _ in range(20):
     y[np.random.randint(200)] = 1
+# 原始数据散点图
 plt.scatter(X[y == 0, 0], X[y == 0, 1])
 plt.scatter(X[y == 1, 0], X[y == 1, 1])
 plt.show()
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
 
+# 使用 sk-learn 中的逻辑回归
 log_reg = LogisticRegression()
 log_reg.fit(X_train, y_train)
-print('未处理数据时的训练集评分:', log_reg.score(X_train, y_train))
-print('未处理数据时的测试集评分:', log_reg.score(X_test, y_test))
+print('sk-learn 中的训练集评分:', log_reg.score(X_train, y_train))
+print('sk-learn 中的测试集评分:', log_reg.score(X_test, y_test))
 
-
+# 绘制决策边界
 def plot_decision_boundary(model, axis):
     x0, x1 = np.meshgrid(
         np.linspace(axis[0], axis[1], int(
@@ -36,12 +38,12 @@ def plot_decision_boundary(model, axis):
     zz = y_predict.reshape(x0.shape)
     custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
     plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
-# 正则化前的决策边界
 plot_decision_boundary(log_reg, axis=[-4, 4, -4, 4])
 plt.scatter(X[y==0,0], X[y==0,1])
 plt.scatter(X[y==1,0], X[y==1,1])
 plt.show()
 
+# 使用多项式逻辑回归
 def PolynomialLogisticRegression(degree):
     return Pipeline([
         ('poly', PolynomialFeatures(degree=degree)),
@@ -50,5 +52,59 @@ def PolynomialLogisticRegression(degree):
     ])
 poly_log_reg = PolynomialLogisticRegression(degree=2)
 poly_log_reg.fit(X_train, y_train)
-print('训练集评分:',poly_log_reg.score(X_train, y_train))
-print('测试集评分:',poly_log_reg.score(X_test, y_test))
+print('多项式逻辑回归训练集评分:',poly_log_reg.score(X_train, y_train))
+print('多项式逻辑回归测试集评分:',poly_log_reg.score(X_test, y_test))
+# 绘制多项式逻辑回归决策边界
+plot_decision_boundary(poly_log_reg, axis=[-4, 4, -4, 4])
+plt.scatter(X[y==0,0], X[y==0,1])
+plt.scatter(X[y==1,0], X[y==1,1])
+plt.show()
+
+# 提高多项式阶数为20后，发生过拟合
+poly_log_reg2 = PolynomialLogisticRegression(degree=20)
+poly_log_reg2.fit(X_train, y_train)
+print('20阶多项式拟合训练集评分:', poly_log_reg2.score(X_train, y_train))
+print('20阶多项式拟合测试集评分:', poly_log_reg2.score(X_test, y_test))
+# 绘制20阶多项式拟合决策边界
+plot_decision_boundary(poly_log_reg2, axis=[-4, 4, -4, 4])
+plt.scatter(X[y==0,0], X[y==0,1])
+plt.scatter(X[y==1,0], X[y==1,1])
+plt.show()
+
+# 通过超参数 C 将模型正则化
+def PolynomialLogisticRegression(degree, C):
+    return Pipeline([
+        ('poly', PolynomialFeatures(degree=degree)),
+        ('std_scaler', StandardScaler()),
+        ('log_reg', LogisticRegression(C=C))
+    ])
+
+poly_log_reg3 = PolynomialLogisticRegression(degree=20, C=0.1)
+poly_log_reg3.fit(X_train, y_train)
+print('多项式逻辑回归训练集评分:',poly_log_reg.score(X_train, y_train))
+print('多项式逻辑回归测试集评分:',poly_log_reg.score(X_test, y_test))
+# 绘制正则化后的20阶多项式拟合决策边界
+plot_decision_boundary(poly_log_reg3, axis=[-4, 4, -4, 4])
+plt.scatter(X[y==0,0], X[y==0,1])
+plt.scatter(X[y==1,0], X[y==1,1])
+plt.show()
+
+
+# 增加 penalty 参数
+def PolynomialLogisticRegression(degree, C, penalty='l2'):
+    return Pipeline([
+        ('poly', PolynomialFeatures(degree=degree)),
+        ('std_scaler', StandardScaler()),
+        ('log_reg', LogisticRegression(C=C, penalty=penalty))
+    ])
+
+# 使用 L1 正则项
+poly_log_reg4 = PolynomialLogisticRegression(degree=20, C=0.1, penalty='l1')
+poly_log_reg4.fit(X_train, y_train)
+print('L1 正则项训练集评分:', poly_log_reg4.score(X_train, y_train))
+print('L1 正则项测试集评分:', poly_log_reg4.score(X_test, y_test))
+# 绘制 L1 正则项决策边界
+plot_decision_boundary(poly_log_reg4, axis=[-4, 4, -4, 4])
+plt.scatter(X[y==0,0], X[y==0,1])
+plt.scatter(X[y==1,0], X[y==1,1])
+plt.show()
