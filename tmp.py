c08454fd47b27a6b16f69cfca8fcf588a7e62952
Tenzu <huangxin.cliff@gmail.com>
2022-06-22 13:49:29 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,22 +1,16 @@
 # coding:utf-8
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-iris = datasets.load_iris()
-X = iris.data[:,:2]
-y = iris.target
+digits = datasets.load_digits()
+X = digits.data
+y = digits.target.copy()
+# 手动生成有偏数据
+y[digits.target==9] = 1
+y[digits.target!=9] = 0
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
-# LogisticRegression 默认使用 OvR
 log_reg = LogisticRegression()
-
-from sklearn.multiclass import OneVsRestClassifier
-ovr = OneVsRestClassifier(log_reg)
-ovr.fit(X_train, y_train)
-print('OvR score:', ovr.score(X_test, y_test))
-from sklearn.multiclass import OneVsOneClassifier
-ovo = OneVsOneClassifier(log_reg)
-ovo.fit(X_train, y_train)
-print('OvO score:', ovo.score(X_test, y_test))
+log_reg.fit(X_train, y_train)
+log_reg.score(X_test, y_test)
