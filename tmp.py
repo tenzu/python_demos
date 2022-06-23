4c8403eb95e772cc38473f799e01e5826541a326
Tenzu <huangxin.cliff@gmail.com>
2022-06-23 09:25:38 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -5,6 +5,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve
 
 digits = datasets.load_digits()
 X = digits.data
@@ -27,4 +28,9 @@ plt.show()
 
 # 精准率-召回率曲线，自动选择坐标值上下限，召回率急剧下降前精确率的值（横坐标）对应平衡点
 plt.plot(precisions, recalls)
+plt.show()
+
+# 绘制 ROC 曲线
+fprs, tprs, thresholds = roc_curve(y_test, decision_scores)
+plt.plot(fprs, tprs)
 plt.show()
