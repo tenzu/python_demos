94a57a20d532bf7b0e517578826648f765e191c7
Tenzu <huangxin.cliff@gmail.com>
2022-06-27 12:03:34 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -2,58 +2,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC
 
-# 数据集包含100个样本，每个样本有2个特征
-# 手动添加噪声特征
 X, y = datasets.make_moons(noise=0.15, random_state=666)
-# 绘制数据集
-plt.scatter(X[y == 0, 0], X[y == 0, 1])
-plt.scatter(X[y == 1, 0], X[y == 1, 1])
-plt.show()
-
-# 使用多项式特征的 SVM
-def PolynomialSVC(degree, C=1.0):
-    return Pipeline([("poly", PolynomialFeatures(degree=degree)),
-                     ("std_scaler", StandardScaler()),
-                     ("linearSVC", LinearSVC(C=C))])
-
-# 3阶多项式
-poly_svc = PolynomialSVC(degree=3)
-poly_svc.fit(X, y)
-
-def plot_decision_boundary(model, axis):
-    x0, x1 = np.meshgrid(
-        np.linspace(axis[0], axis[1], int(
-            (axis[1] - axis[0]) * 100)).reshape(-1, 1),
-        np.linspace(axis[2], axis[3], int(
-            (axis[3] - axis[2]) * 100)).reshape(-1, 1),
-    )
-    X_new = np.c_[x0.ravel(), x1.ravel()]
-    y_predict = model.predict(X_new)
-    zz = y_predict.reshape(x0.shape)
-    from matplotlib.colors import ListedColormap
-    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
-    plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
-
-# 绘制3阶多项式的决策边界
-plot_decision_boundary(poly_svc, axis=[-1.5, 2.5, -1.0, 1.5])
-plt.scatter(X[y == 0, 0], X[y == 0, 1])
-plt.scatter(X[y == 1, 0], X[y == 1, 1])
-plt.show()
-
-# 使用多项式核函数的 SVM（替代多项式特征）
-def PolynomialKernelSVC(degree, C=1.0):
-    return Pipeline([("std_scaler", StandardScaler()),
-                     ("kernelSVC", SVC(kernel="poly", degree=degree, C=C))])
-
-poly_kernel_svc = PolynomialKernelSVC(degree=3)
-poly_kernel_svc.fit(X, y)
-# 绘制使用核函数后的决策边界
-plot_decision_boundary(poly_kernel_svc, axis=[-1.5, 2.5, -1.0, 1.5])
-plt.scatter(X[y == 0, 0], X[y == 0, 1])
-plt.scatter(X[y == 1, 0], X[y == 1, 1])
-plt.show()
