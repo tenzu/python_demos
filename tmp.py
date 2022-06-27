a946caea60851a78dd04c0f7bee9b0ffb02ddf77
Tenzu <huangxin.cliff@gmail.com>
2022-06-27 09:39:18 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -2,33 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_score
-from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
 
-digits = datasets.load_digits()
-X = digits.data
-y = digits.target.copy()
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
-
-log_reg = LogisticRegression()
-log_reg.fit(X_train, y_train)
-y_predict = log_reg.predict(X_test)
-decision_scores = log_reg.decision_function(X_test)
-
-# 修改 average 参数（默认为 binary），计算多分类精准率
-print('多分类精准率:\n', precision_score(y_test, y_predict, average="micro"))
-# 混淆矩阵
-print('Confusion Matrix:\n', confusion_matrix(y_test, y_predict))
-# 绘制混淆矩阵，显示预测正确的位置
-cfm = confusion_matrix(y_test, y_predict)
-plt.matshow(cfm, cmap=plt.cm.gray)
-plt.show()
-
-# 绘制混淆矩阵，显示预测错误的地方
-row_sums = np.sum(cfm, axis=1)
-err_matrix = cfm / row_sums
-np.fill_diagonal(err_matrix, 0)
-plt.matshow(err_matrix, cmap=plt.cm.gray)
-plt.show()
+iris = datasets.load_iris()
+X = iris.data
+y = iris.targe
+X = X[y<2,:2]
+y = y[y<2]
+# 不进行预测，因此不分成训练集和数据集
+standardScaler = StandardScaler()
+standardScaler.fit(X)
+X_standard = standardScaler.transform(X)
