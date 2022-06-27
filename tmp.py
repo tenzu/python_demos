7c83ea2fe2748010edf3db40950ab186a11ea146
Tenzu <huangxin.cliff@gmail.com>
2022-06-27 09:41:13 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
 
 iris = datasets.load_iris()
 X = iris.data
@@ -13,3 +14,28 @@ y = y[y<2]
 standardScaler = StandardScaler()
 standardScaler.fit(X)
 X_standard = standardScaler.transform(X)
+
+# C 为 soft margin 参数，取值越大越接近 hard margin
+svc = LinearSVC(C=1e9)
+svc.fit(X_standard, y)
+
+def plot_decision_boundary(model, axis):
+    
+    x0, x1 = np.meshgrid(
+        np.linspace(axis[0], axis[1], int((axis[1]-axis[0])*100)).reshape(-1, 1),
+        np.linspace(axis[2], axis[3], int((axis[3]-axis[2])*100)).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#EF9A9A','#FFF59D','#90CAF9'])
+    
+    plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
+
+plot_decision_boundary(svc, axis=[-3, 3, -3, 3])
+plt.scatter(X_standard[y==0,0], X_standard[y==0,1])
+plt.scatter(X_standard[y==1,0], X_standard[y==1,1])
+plt.show()
