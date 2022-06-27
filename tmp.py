55f5438e7a493e28cb210c76a3385272b614de75
Tenzu <huangxin.cliff@gmail.com>
2022-06-27 12:16:41 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -2,5 +2,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
 
 X, y = datasets.make_moons(noise=0.15, random_state=666)
+
+def RBFKernelSVC(gamma):
+    return Pipeline([("std_scaler", StandardScaler()),
+                     ("svc", SVC(kernel="rbf", gamma=gamma))])
+
+# 可观察 gamma 取 100、10、1 时候决策边界的变化
+# gamma 较大时会发生过拟合，较小时会发生欠拟合
+# gamma=0.1 时接近线性决策边界
+svc = RBFKernelSVC(gamma=1)
+svc.fit(X, y)
+
+def plot_decision_boundary(model, axis):
+    x0, x1 = np.meshgrid(
+        np.linspace(axis[0], axis[1], int(
+            (axis[1] - axis[0]) * 100)).reshape(-1, 1),
+        np.linspace(axis[2], axis[3], int(
+            (axis[3] - axis[2]) * 100)).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
+    plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
+
+plot_decision_boundary(svc, axis=[-1.5, 2.5, -1.0, 1.5])
+plt.scatter(X[y == 0, 0], X[y == 0, 1])
+plt.scatter(X[y == 1, 0], X[y == 1, 1])
+plt.show()
