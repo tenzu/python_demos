3b58ea36f8553eaab6e52750a2cdee5f4f24c4fd
Tenzu <huangxin.cliff@gmail.com>
2022-06-28 10:07:44 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -12,3 +12,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
 dt_reg = DecisionTreeRegressor()
 dt_reg.fit(X_train, y_train)
 
+# 训练集准确度很好但测试集很差，说明过拟合，模型泛化能力差
+print('Decison tree regressor score on test data:\n', dt_reg.score(X_test, y_test))
+print('Decison tree regressor score on train data:\n', dt_reg.score(X_train, y_train))
