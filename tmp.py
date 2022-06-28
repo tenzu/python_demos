76d1ac96269085979d3b9cf833e004ea84e20406
Tenzu <huangxin.cliff@gmail.com>
2022-06-28 10:05:25 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -2,28 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 
-X, y = datasets.make_moons(noise=0.25, random_state=666)
-# max_depth, min_samples_split, min_sample_leaf, max_leaf_nodes 都是超参数
-dt_clf = DecisionTreeClassifier(max_depth=2)
-dt_clf.fit(X, y)
+boston = datasets.load_boston()
+X = boston.data
+y = boston.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
+dt_reg = DecisionTreeRegressor()
+dt_reg.fit(X_train, y_train)
 
-def plot_decision_boundary(model, axis):
-    x0, x1 = np.meshgrid(
-        np.linspace(axis[0], axis[1], int(
-            (axis[1] - axis[0]) * 100)).reshape(-1, 1),
-        np.linspace(axis[2], axis[3], int(
-            (axis[3] - axis[2]) * 100)).reshape(-1, 1),
-    )
-    X_new = np.c_[x0.ravel(), x1.ravel()]
-    y_predict = model.predict(X_new)
-    zz = y_predict.reshape(x0.shape)
-    from matplotlib.colors import ListedColormap
-    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
-    plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
-
-plot_decision_boundary(dt_clf, axis=[-1.5, 2.5, -1.0, 1.5])
-plt.scatter(X[y == 0, 0], X[y == 0, 1])
-plt.scatter(X[y == 1, 0], X[y == 1, 1])
-plt.show()
