d60071f1c6160fe0ab3255f1b3a59b763cc17cc7
Tenzu <huangxin.cliff@gmail.com>
2022-06-28 11:25:23 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,14 +1,15 @@
 # coding:utf-8
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
 
 X, y = datasets.make_moons(n_samples=500, noise=0.3, random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
+# 使用 OOB 后无需 train_test_split
 bagging_clf = BaggingClassifier(DecisionTreeClassifier(),
-                           n_estimators=500, max_samples=100,
-                           bootstrap=True)
-bagging_clf.fit(X_train, y_train)
-print('决策树放回取样准确度:\n', bagging_clf.score(X_test, y_test))
+                                n_estimators=500,
+                                max_samples=100,
+                                bootstrap=True,
+                                oob_score=True)
+bagging_clf.fit(X, y)
+print('决策树放回取样 OOB 准确度:\n', bagging_clf.oob_score_)
