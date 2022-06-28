601e44750d5812ead91afc8e9a3d62968cbe4a8a
Tenzu <huangxin.cliff@gmail.com>
2022-06-28 10:01:51 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -4,32 +4,28 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.tree import DecisionTreeClassifier
 
-iris = datasets.load_iris()
-# 仅取后两种特征
-X = iris.data[:, 2:]
-y = iris.target
-# 决策树深度、criterion 都是超参数
-dt_clf = DecisionTreeClassifier(max_depth=2,
-                                criterion="entropy",
-                                random_state=42)
+X, y = datasets.make_moons(noise=0.25, random_state=666)
+# max_depth, min_samples_split, min_sample_leaf, max_leaf_nodes 都是超参数
+dt_clf = DecisionTreeClassifier(max_depth=2)
 dt_clf.fit(X, y)
 
 def plot_decision_boundary(model, axis):
+    
     x0, x1 = np.meshgrid(
-        np.linspace(axis[0], axis[1], int(
-            (axis[1] - axis[0]) * 100)).reshape(-1, 1),
-        np.linspace(axis[2], axis[3], int(
-            (axis[3] - axis[2]) * 100)).reshape(-1, 1),
+        np.linspace(axis[0], axis[1], int((axis[1]-axis[0])*100)).reshape(-1, 1),
+        np.linspace(axis[2], axis[3], int((axis[3]-axis[2])*100)).reshape(-1, 1),
     )
     X_new = np.c_[x0.ravel(), x1.ravel()]
+
     y_predict = model.predict(X_new)
     zz = y_predict.reshape(x0.shape)
-    from matplotlib.colors import ListedColormap
-    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
-    plt.contourf(x0, x1, zz, cmap=custom_cmap)
 
-plot_decision_boundary(dt_clf, axis=[0.5, 7.5, 0, 3])
-plt.scatter(X[y == 0, 0], X[y == 0, 1])
-plt.scatter(X[y == 1, 0], X[y == 1, 1])
-plt.scatter(X[y == 2, 0], X[y == 2, 1])
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#EF9A9A','#FFF59D','#90CAF9'])
+    
+    plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
+
+plot_decision_boundary(dt_clf, axis=[-1.5, 2.5, -1.0, 1.5])
+plt.scatter(X[y==0,0], X[y==0,1])
+plt.scatter(X[y==1,0], X[y==1,1])
 plt.show()
