09fb9bb0fc84fd9afe7f746b739147437793f3ad
tenzu <huangxin.cliff@gmail.com>
2023-04-14 17:31:34 +0800

    formmated code

--- luckyDraw.py
+++ luckyDraw.py
@@ -4,8 +4,10 @@ import random
 
 times = input("Lucky draw times: ")
 pool = [1, 2, 3, 4, 5]
-award = ["30 min screen time", "15 min screen time",
-         "No award", "15 min study time", "30 min study time"]
+award = [
+    "30 min screen time", "15 min screen time", "No award",
+    "15 min study time", "30 min study time"
+]
 i = 1
 j = len(pool)
 while i <= int(times):
@@ -17,4 +19,4 @@ while i <= int(times):
     i += 1
 print("\nThe best award is:", award[j - 1])
 
-#a = input("\n")
+# a = input("\n")
